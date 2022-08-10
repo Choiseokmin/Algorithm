7f6a4f6afe363be085a1e2213468dafdc6e0ba55
SeokMin <choiseokmin@naver.com>
2022-08-10 13:32:20 +0900

    Add baekjoon file

---
+++ 단계별 문제/01.입출력과사칙연산/3003.py
@@ -0,0 +1,31 @@
+a, b, c, d, e, f = map(int, input().split())
+
+if(a == 1):
+    print("0", end=' ')
+elif(a != 1):
+    print(1-a, end=' ')
+
+if(b == 1):
+    print("0", end=' ')
+elif(b != 1):
+    print(1-b, end=' ')
+    
+if(c == 2):
+    print("0", end=' ')
+elif(c != 2):
+    print(2-c, end=' ')
+    
+if(d == 2):
+    print("0", end=' ')
+elif(d != 2):
+    print(2-d, end=' ')
+    
+if(e == 2):
+    print("0", end=' ')
+elif(e != 2):
+    print(2-e, end=' ')
+    
+if(f == 8):
+    print("0", end=' ')
+elif(f != 8):
+    print(8-f, end=' ')
